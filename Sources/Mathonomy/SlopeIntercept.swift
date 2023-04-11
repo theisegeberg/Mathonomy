b614ef6a48d02838cb66bfccfdbcd1faf2bcb214
Theis Egeberg <theis.egeberg@gmail.com>
2023-04-11 08:48:46 +0200

    Added more complex slope intercept methods

--- Sources/Mathonomy/SlopeIntercept.swift
+++ Sources/Mathonomy/SlopeIntercept.swift
@@ -3,9 +3,22 @@ import CoreGraphics
 
 extension CGPoint {
     
+    public func slope(for other:CGPoint) -> Double {
+        (other.y - self.y) / (other.x - self.x)
+    }
+    
+    public func yIntercept(forSlope slope:Double) -> Double {
+        self.y - (slope * self.x)
+    }
+    
     public func slopeIntercept(for other: CGPoint) -> (slope:Double, yIntercept:Double) {
-        let slope = (other.y - self.y) / (other.x - self.x)
-        return (slope: slope, yIntercept: self.y - (slope * self.x))
+        let slope = slope(for: other)
+        return (slope: slope, yIntercept: yIntercept(forSlope: slope))
+    }
+    
+    public func slopeIntercept(for other: CGPoint, calculatingYforX x:Double) -> (Double) {
+        let slope = slope(for: other)
+        return slope * x + yIntercept(forSlope: slope)
     }
     
 }
